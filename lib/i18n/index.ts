62fbcfd03acdf0020ebb432f7864d7bbbe3b04f2
DavidIsA10But <david@10but.com>
2025-08-15 14:14:15 +0200

    Add files via upload

--- lib/i18n/index.ts
+++ lib/i18n/index.ts
@@ -2,7 +2,7 @@ import FR from "./fr";
 import EN from "./en";
 
 export type LocaleKey = "fr" | "en";
-export type Messages = typeof FR; // même structure pour EN
+export type Messages = typeof FR | typeof EN;
 
 export const getMessages = (locale: LocaleKey): Messages =>
   locale === "en" ? EN : FR;
